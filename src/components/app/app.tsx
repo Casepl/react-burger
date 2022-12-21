58c820068b90070a3e5f9e7bf9366e19cec25acc
Sergey Scherbachenko <sshcherbachenko@avito.ru>
2022-12-22 02:29:38 +0300

    Добавлен деплой

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -49,7 +49,7 @@ function App() {
 
   return (
     <div className={styles.root}>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <ModalRoutes />
       </Router>
       {error &&
