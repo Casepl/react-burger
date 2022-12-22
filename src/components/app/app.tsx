04345ce8c2a9b8f5fe7d5c702398bcb89c7ce190
Sergey Scherbachenko <sshcherbachenko@avito.ru>
2022-12-22 09:12:05 +0300

    Убран base урл

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -49,7 +49,7 @@ function App() {
 
   return (
     <div className={styles.root}>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <ModalRoutes />
       </Router>
       {error &&
